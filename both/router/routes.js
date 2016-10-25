daae42664fdd38c9a8c6ebd6b37296ea1fea66c4
kfkogasa <kfkogasa@hawaii.edu>
2016-10-24 16:57:57 -1000

    created/edited templates, added routes

--- both/router/routes.js
+++ both/router/routes.js
@@ -8,4 +8,12 @@ Router.route('/', {
 Router.route('/overview', {
   name: 'overview'
 });
-
+Router.route('/myOverview', {
+  name: 'myOverview'
+});
+Router.route('/graph', {
+  name: 'graph'
+});
+Router.route('/cardDetails', {
+  name: 'cardDetails'
+});
