a710bfb70839b2e69b6177967a35092c3af65c28
kfkogasa <kfkogasa@hawaii.edu>
2016-11-08 20:08:45 -1000

    timer

--- client/templates/cardDetails/cardDetails.js
+++ client/templates/cardDetails/cardDetails.js
@@ -13,4 +13,38 @@ Template.cardDetails.onRendered(function () {
   if (card.issue != false) {
     document.getElementById('issue').checked = true;
   }
+
+  var timer;
+ // var time = 0;
+});
+
+Template.cardDetails.events({
+  "click #start": function () {
+    timer = Meteor.setInterval(function () {
+      document.getElementById('time').innerHTML = Number(document.getElementById('time').innerHTML) + 1;
+      if(document.getElementById('sec').innerHTML == 59) {
+        if(document.getElementById('min').innerHTML == 59) {
+          document.getElementById('min').innerHTML = 0;
+          document.getElementById('hour').innerHTML = Number(document.getElementById('hour').innerHTML) + 1;
+        }
+        else {
+          document.getElementById('min').innerHTML = Number(document.getElementById('min').innerHTML) + 1;
+        }
+        document.getElementById('sec').innerHTML = 0;
+      }
+      else {
+        document.getElementById('sec').innerHTML = Number(document.getElementById('sec').innerHTML) + 1;
+      }
+    }, 1000);
+  },
+
+  "click #stop": function () {
+    //stop timer
+    Meteor.clearInterval(timer);
+    var cardId = Session.get("selectedCard");
+    console.log(cardId);
+    var time = Number(document.getElementById('time').innerHTML);
+    //add time to time in DB
+    Meteor.call("updateCardTime", cardId, time);
+  }
 });
--- server/methods/cards.js
+++ server/methods/cards.js
@@ -2,13 +2,25 @@ Meteor.methods({
   /**
    * Creates task card entry
    */
-  addCard: function(name, description) {
+  addCard: function (name, description) {
     Cards.insert({
       name: name,
       description: description,
       category: 'toDo',
       user: null,
-      issue: false
+      issue: false,
+      time: 0
+    });
+  },
+
+  /**
+   * Updates time for card
+   */
+  updateCardTime: function (id, time) {
+    Cards.update({_id: id}, {
+      $inc: {
+        time: time
+      }
     });
   }
 });
