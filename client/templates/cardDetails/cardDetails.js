3a3f44b742f732f8f8718e029f64f02db8fb2476
kfkogasa <kfkogasa@hawaii.edu>
2016-11-25 17:03:30 -1000

    timer
    fixed bug in timer and added total time (which updates upon stoping the
    timer)

--- client/templates/cardDetails/cardDetails.js
+++ client/templates/cardDetails/cardDetails.js
@@ -15,15 +15,30 @@ Template.cardDetails.onRendered(function () {
   }
 
   var timer;
- // var time = 0;
+  // var time = 0;
+
+  //load current DB time
+  var cardTime = card.time;
+
+  document.getElementById('currTime').innerHTML = cardTime;
+
+  var sec = cardTime % 60;
+  cardTime = (cardTime - sec) / 60;
+  var min = cardTime % 60;
+  cardTime = (cardTime - min) / 60;
+  var hour = cardTime;
+
+  document.getElementById('currentHour').innerHTML = hour;
+  document.getElementById('currentMin').innerHTML = min;
+  document.getElementById('currentSec').innerHTML = sec;
 });
 
 Template.cardDetails.events({
   "click #start": function () {
     timer = Meteor.setInterval(function () {
       document.getElementById('time').innerHTML = Number(document.getElementById('time').innerHTML) + 1;
-      if(document.getElementById('sec').innerHTML == 59) {
-        if(document.getElementById('min').innerHTML == 59) {
+      if (document.getElementById('sec').innerHTML == 59) {
+        if (document.getElementById('min').innerHTML == 59) {
           document.getElementById('min').innerHTML = 0;
           document.getElementById('hour').innerHTML = Number(document.getElementById('hour').innerHTML) + 1;
         }
@@ -42,9 +57,32 @@ Template.cardDetails.events({
     //stop timer
     Meteor.clearInterval(timer);
     var cardId = Session.get("selectedCard");
-    console.log(cardId);
     var time = Number(document.getElementById('time').innerHTML);
+
     //add time to time in DB
     Meteor.call("updateCardTime", cardId, time);
+
+    //update current time
+    var currTime = Number(document.getElementById('currTime').innerHTML);
+    console.log(currTime);
+    currTime = currTime + time;
+    console.log(currTime);
+    document.getElementById('currTime').innerHTML = currTime;
+    var newSec = currTime % 60;
+    currTime = (currTime - newSec) / 60;
+    console.log(currTime);
+    var newMin = currTime % 60;
+    currTime = (currTime - newMin) / 60;
+    var newHour = currTime;
+
+    document.getElementById('currentHour').innerHTML = newHour;
+    document.getElementById('currentMin').innerHTML = newMin;
+    document.getElementById('currentSec').innerHTML = newSec;
+
+    //reset timer
+    document.getElementById('time').innerHTML = 0;
+    document.getElementById('hour').innerHTML = 0;
+    document.getElementById('min').innerHTML = 0;
+    document.getElementById('sec').innerHTML = 0;
   }
 });
