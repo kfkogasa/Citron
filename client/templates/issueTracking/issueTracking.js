880f3b2f51a179a750479c1597819c9549d52efc
Chunmeista <barryn.chun@gmail.com>
2016-11-28 11:56:56 -1000

    issue tracking page added

--- client/templates/issueTracking/issueTracking.js
+++ client/templates/issueTracking/issueTracking.js
@@ -1,3 +1,39 @@
-/**
- * Created by Chunmeista on 11/28/2016.
- */
+Template.issueTracking.events({
+    /**
+     * redirects to cardDetail template which will display clicked card's info
+     */
+
+    "click .card": function() {
+        var cardId = this._id;
+        //save clicked card's id
+        Session.set("selectedCard",cardId);
+        Router.go('cardDetails');
+    },
+    "click .cardIssue": function() {
+        var cardId = this._id;
+        //save clicked card's id
+        Session.set("selectedCard",cardId);
+        Router.go('cardDetails');
+    }
+});
+
+Template.issueTracking.helpers({
+    /**
+     * returns Object containing toDo data from Cards collection that are issues
+     */
+    'toDoIssue': function() {
+        return Cards.find({category: "toDo", user: Meteor.users.findOne(Meteor.userId()).username, issue: true});
+    },
+    /**
+     * returns Object containing inProgress data from Cards collection that are issues
+     */
+    'inProgressIssue': function() {
+        return Cards.find({category: "inProgress", user: Meteor.users.findOne(Meteor.userId()).username, issue: true});
+    },
+    /**
+     * returns Object containing codeReview data from Cards collection that are issues
+     */
+    'codeReviewIssue': function() {
+        return Cards.find({category: "codeReview", user: Meteor.users.findOne(Meteor.userId()).username, issue: true});
+    },
+});
